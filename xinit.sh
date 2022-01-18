7222d91453900c266fd3d5726605e3887d7e2dc6
Aaron Hayden <ahayden@users.noreply.github.com>
2022-01-18 03:35:07 +0000

    Disable VNC auth

--- xinit.sh
+++ xinit.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 Xvfb :10 -screen 0 1366x768x24 &
-x11vnc -ncache 0 -passwd fixthisbug -display :10 -N -forever &
+x11vnc -ncache 0 -nopw -display :10 -N -forever &
 sleep 5 
 xterm &
 wmaker
